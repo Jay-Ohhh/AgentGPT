7db6f92a1c0e2b32c3e567a18c86098465bf0efb
Jay <1598353326@qq.com>
2023-05-05 22:16:09 +0800

    chore

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,14 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "../env/server.mjs";
+/**
+ * @see https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#prevent-hot-reloading-from-creating-new-instances-of-prismaclient
+ */
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient; };
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
+export const prisma = globalForPrisma.prisma ||
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma;
